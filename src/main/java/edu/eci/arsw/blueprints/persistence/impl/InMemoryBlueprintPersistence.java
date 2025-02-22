85fc3a326279e5410f9b0af17d379445f9a78d50
JuanPabl07DP <juanpadazer15@gmail.com>
2025-02-22 16:41:15 -0500

    Autores corregidos

--- src/main/java/edu/eci/arsw/blueprints/persistence/impl/InMemoryBlueprintPersistence.java
+++ src/main/java/edu/eci/arsw/blueprints/persistence/impl/InMemoryBlueprintPersistence.java
@@ -28,7 +28,7 @@ public class InMemoryBlueprintPersistence implements BlueprintsPersistence{
                 new Point(140, 140),
                 new Point(115, 115)
         };
-        Blueprint bp1 = new Blueprint("Juan", "House", pts1);
+        Blueprint bp1 = new Blueprint("juan", "house", pts1);
         blueprints.put(new Tuple<>(bp1.getAuthor(), bp1.getName()), bp1);
 
         // Plano 2 - Autor 1
@@ -37,7 +37,7 @@ public class InMemoryBlueprintPersistence implements BlueprintsPersistence{
                 new Point(215, 215),
                 new Point(230, 230)
         };
-        Blueprint bp2 = new Blueprint("Pablo", "School", pts2);
+        Blueprint bp2 = new Blueprint("pablo", "school", pts2);
         blueprints.put(new Tuple<>(bp2.getAuthor(), bp2.getName()), bp2);
 
         // Plano 3 - Autor 2
@@ -47,7 +47,7 @@ public class InMemoryBlueprintPersistence implements BlueprintsPersistence{
                 new Point(330, 330),
                 new Point(320, 320)
         };
-        Blueprint bp3 = new Blueprint("Nicolas", "Office", pts3);
+        Blueprint bp3 = new Blueprint("nicolas", "office", pts3);
         blueprints.put(new Tuple<>(bp3.getAuthor(), bp3.getName()), bp3);
 
         // Plano 4 - Autor 3
@@ -58,7 +58,7 @@ public class InMemoryBlueprintPersistence implements BlueprintsPersistence{
                 new Point(420, 420),
                 new Point(425, 425)
         };
-        Blueprint bp4 = new Blueprint("Fabio", "Mall", pts4);
+        Blueprint bp4 = new Blueprint("fabio", "mall", pts4);
         blueprints.put(new Tuple<>(bp4.getAuthor(), bp4.getName()), bp4);
     }
 
