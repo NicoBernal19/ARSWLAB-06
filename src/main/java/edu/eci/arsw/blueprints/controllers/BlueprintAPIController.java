2dbb8c3c7b39b304922120e269a7e05d5192cade
JuanPabl07DP <juanpadazer15@gmail.com>
2025-02-22 16:21:36 -0500

    Controller OK

--- src/main/java/edu/eci/arsw/blueprints/controllers/BlueprintAPIController.java
+++ src/main/java/edu/eci/arsw/blueprints/controllers/BlueprintAPIController.java
@@ -1,26 +1,37 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package edu.eci.arsw.blueprints.controllers;
 
-import java.util.LinkedHashSet;
-import java.util.Set;
+import edu.eci.arsw.blueprints.model.Blueprint;
+import edu.eci.arsw.blueprints.services.BlueprintsServices;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author hcadavid
+ * @author Juan Pablo Daza Pereira
  */
+@RestController
+@RequestMapping(value = "/blueprints")
 public class BlueprintAPIController {
-    
-    
-    
-    
-    
+    @Autowired
+    BlueprintsServices blueprintsServices;
+
+    @RequestMapping(method = RequestMethod.GET)
+    public ResponseEntity<?> getAllBlueprints() {
+        try {
+            Set<Blueprint> blueprints = blueprintsServices.getAllBlueprints();
+            return new ResponseEntity<>(blueprints, HttpStatus.ACCEPTED);
+        }catch (Exception ex){
+            Logger.getLogger(BlueprintAPIController.class.getName()).log(Level.SEVERE, null, ex);
+            return new ResponseEntity<>("Error", HttpStatus.NOT_FOUND);
+        }
+    }
 }
 
